8c80cf90dcb900b4851d3f6c1078b904367dc667
Fariduddin <m.fariduddinfakhrizan@gmail.com>
2026-01-02 22:53:24 +0800

    Initial commit: Spring Boot project with YAML configuration

---
+++ neuro-wallet/src/main/java/com/neurogine/wallet/NeuroWalletApplication.java
@@ -0,0 +1,13 @@
+package com.neurogine.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroWalletApplication.class, args);
+	}
+
+}
---
+++ neuro-wallet/src/test/java/com/neurogine/wallet/NeuroWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurogine.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
