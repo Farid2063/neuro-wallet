cc260f5e80bd45f99f0001a17ab08684a192e5e3
Fariduddin <m.fariduddinfakhrizan@gmail.com>
2026-01-05 12:43:25 +0800

    Enhance Frontend and fixed return user data

--- neuro-wallet/src/main/java/com/neurogine/wallet/controller/WalletController.java
+++ neuro-wallet/src/main/java/com/neurogine/wallet/controller/WalletController.java
@@ -1,7 +1,12 @@
 package com.neurogine.wallet.controller;
 
 import com.neurogine.wallet.dto.TransferRequest;
+import com.neurogine.wallet.entity.TransactionRecord;
+import com.neurogine.wallet.repository.TransactionRepository;
 import com.neurogine.wallet.service.WalletService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +15,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*") // Allows your Vue 3 frontend to connect
 public class WalletController {
     @Autowired private WalletService walletService;
+    @Autowired private TransactionRepository transactionRepository;
 
     @PostMapping("/transfer")
     public String transfer(@RequestBody TransferRequest request) {
         return walletService.transfer(request.getFromWalletId(), request.getToWalletId(), request.getAmount());
+    }
+
+    @GetMapping("/history/{userId}")
+    public List<TransactionRecord> getTransactionHistory(@PathVariable Long userId) {
+        return transactionRepository.findBySenderIdOrReceiverId(userId, userId);
     }
 }
